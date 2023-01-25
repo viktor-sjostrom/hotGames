9a2167510f8da67fa57e5fe146053f9d3008864e
Viktor Sjöström <v.sjostrom93@gmail.com>
2023-01-25 20:32:34 +0100

    added nr of games (wrong nr) and moved published year

--- js/hotGames.js
+++ js/hotGames.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
 
-    // Här anropar ni funktionen getGames med argumentet true. Funktionen getGames tar en parameter (all) som, om den är sann, innebär att vi vill visa alla spel på heta listan, men om den är falsk bara vill visa spel som vi vet är utgivna. (Se vidare punkt 5 i uppgiftsbeskrivningen under "Kom igång med uppgiften").
+    // Här anropar ni funktionen getGames med argumentet true.
+    // Funktionen getGames tar en parameter (all) som, om den är sann, innebär att vi vill visa alla
+    // spel på heta listan, men om den är falsk bara vill visa spel som vi vet är utgivna.
+    // (Se vidare punkt 5 i uppgiftsbeskrivningen under "Kom igång med uppgiften").
     getGames(true);
 
     $("#showAll").on("click", function () {
@@ -10,7 +13,10 @@ $(document).ready(function () {
     $("#onlyPublished").on("click", function () {
         getGames(false);
     })
-    //Här skapar ni en händelsehanterare i jQuery som lyssnar på om användaren klickar i radio-knappen ”Endast publicerad”. I händelse av en sådan klickning ska händelsehanteraren trigga en funktion, som i sin tur ska anropa getGames, men med false som argument. (Se vidare punkt 9 i uppgiftsbeskrivningen under "Kom igång med uppgiften").
+    //Här skapar ni en händelsehanterare i jQuery som lyssnar på om användaren klickar i radio-knappen
+    // ”Endast publicerad”. I händelse av en sådan klickning ska händelsehanteraren trigga en funktion,
+    // som i sin tur ska anropa getGames, men med false som argument. (Se vidare punkt 9 i
+    // uppgiftsbeskrivningen under "Kom igång med uppgiften").
 
 
 });
@@ -42,15 +48,55 @@ function getGames(all) {
                     var gameRank = value.rank;
                     var gamePublished = value.yearPublished;
 
-                    items.push("<article><img src="+ gameThumbnail+" alt='Games thumbnail'><h2>"+gameRank+". "+gameName+"</h2><p>Publicerat: </p>"+gamePublished+"</article>"); //Plocka dynamiskt ut namnet på spelet ur JSON-listan och se till att det visas mellan h2-taggarna. Se till att namnet föregås av rätt placering på heta listan (1, 2, 3 .. n). Plocka dynamiskt ut årtalet då spelet publicerades ur JSON-listan och se till att det visas efter ”Publicerat: ”. Kom ihåg att även uppdatera ev variabler med nya värden här. (Se vidare punkt 7a, 7b och 7c i uppgiftsbeskrivningen under "Kom igång med uppgiften"). Förutom namn, årtal och listplacering på spelen skall även bilder skrivas ut. Externa URL:er till bilderna kan hämtas ut från svaret vi fått från webbtjänsten, mer specifikt som egenskapen thumbnail. Se till att infoga bild-element direkt under föräldraelementet article. (Se vidare punkt 8 i uppgiftsbeskrivningen under "Kom igång med uppgiften").
+                    numberOfGames++;
+
+                    //Plocka dynamiskt ut namnet på spelet ur JSON-listan och se till att det visas mellan h2-taggarna.
+                    // Se till att namnet föregås av rätt placering på heta listan (1, 2, 3 .. n).
+                    // Plocka dynamiskt ut årtalet då spelet publicerades ur JSON-listan och se till att det visas efter
+                    // ”Publicerat: ”. Kom ihåg att även uppdatera ev variabler med nya värden här.
+                    // (Se vidare punkt 7a, 7b och 7c i uppgiftsbeskrivningen under "Kom igång med uppgiften").
+                    // Förutom namn, årtal och listplacering på spelen skall även bilder skrivas ut.
+                    // Externa URL:er till bilderna kan hämtas ut från svaret vi fått från webbtjänsten,
+                    // mer specifikt som egenskapen thumbnail. Se till att infoga bild-element direkt under
+                    // föräldraelementet article. (Se vidare punkt 8 i uppgiftsbeskrivningen under "Kom igång med uppgiften").
+
+                    //Fixa alt - texten
+                    items.push("<article><img src="+ gameThumbnail+" alt='Games thumbnail'><h2>"+gameRank+". "+gameName+"</h2><p>Publicerat: "+gamePublished+"</p></article>");
 
                 });
             } else {
-                //Skriv kod i else-satsen, som reagerar på om getGames inparameter inte är sann. M a o: vad händer om användaren klickat i radio-knappen ”Endast publicerade”? Kom ihåg att även uppdatera ev variabler med nya värden här. (Se vidare punkt 10 i uppgiftsbeskrivningen under "Kom igång med uppgiften").
+
+                //Skriv kod i else-satsen, som reagerar på om getGames inparameter inte är sann.
+                // M a o: vad händer om användaren klickat i radio-knappen ”Endast publicerade”?
+                // Kom ihåg att även uppdatera ev variabler med nya värden här.
+                // (Se vidare punkt 10 i uppgiftsbeskrivningen under "Kom igång med uppgiften").
+
+                $.each(response, function (index, value) {
+                    var gameThumbnail = value.thumbnail;
+                    var gameName = value.name;
+                    var gameRank = value.rank;
+                    var gamePublished = value.yearPublished;
+
+
+                    if(gamePublished !== 2023) {
+                        numberOfGames++;
+                        items.push("<article><img src="+ gameThumbnail+" alt='Games thumbnail'><h2>"+gameRank+". "+gameName+"</h2><p>Publicerat: "+gamePublished+"</p></article>");
+
+                    }
+                });
             }
 
+
             $("#games").html(items.join(""));
-            //Uppdatera sidfoten med antal spel som faktiskt visas. Denna siffra tas fram dynamiskt och förändras därför beroende på vilken radio-knapp som är förkryssad. (Se vidare punkt 11 i uppgiftsbeskrivningen under "Kom igång med uppgiften").
+
+            //Få till så de står "Antal spel" innan nummer + fixa siffran som misstämmer atm
+            $("#noOfGames").html(items.push("<p>Antal spel: "+numberOfGames+"</p>"));
+
+
+            //Uppdatera sidfoten med antal spel som faktiskt visas.
+            // Denna siffra tas fram dynamiskt och förändras därför beroende på
+            // vilken radio-knapp som är förkryssad. (Se vidare punkt 11 i uppgiftsbeskrivningen
+            // under "Kom igång med uppgiften").
         }
 
         //error: function(error) {
